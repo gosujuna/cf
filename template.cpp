97a62476cd781d721206d56e681e63117c8dea5a
Junha Lee <jlee3551@gatech.edu>
2023-02-22 17:17:51 -0500

    initial commit

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T> T pow(T a, long long b) {
+	assert(b >= 0);
+	T r = 1;
+	while (b) {
+		if (b & 1)
+			r *= a;
+		b >>= 1;
+		a *= a;
+	}
+	return r;
+}
+
+int main() { ios_base::sync_with_stdio(false), cin.tie(nullptr); }
